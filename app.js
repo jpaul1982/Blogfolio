3324cb5578155116a8749721e57c519ac5139f3f
Paul Molnar <saintpaul55012@yahoo.com>
2020-01-06 17:50:06 -0600

    data from db rendering on dom

--- app.js
+++ app.js
@@ -4,30 +4,35 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   Blog = require("./models/blog"),
   mongoose = require("mongoose"); // mongoose is an ODM(object data mapper, this allows us to interact with our DB using JS)
-  methodOverride = require("method-override");
+methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/blogfolio", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 
 //////// Index Routes ///////////
 app.get("/", (req, res) => {
-  res.render("index");
+  Blog.find({}, (err, allBlogs) => {
+    if (err) {
+      console.log("Something went wrong:", err);
+    } else {
+      res.render("index", {
+        blogs: allBlogs
+      });
+    }
+  });
 });
+
 app.get("/blogs", (req, res) => {
-    let blogs = [
-        {name: "name", year: 1900},
-        {name: "name", year: 1900},
-        {name: "name", year: 1900}
-    ]
-  res.render("blogs", {blogs:blogs});
 });
 //////// New Routes ///////////
 app.get("/new_post", (req, res) => {
@@ -38,18 +43,21 @@ app.get("/new_post", (req, res) => {
 
 //////// Create Routes //////////
 
-
-app.post("/blogs", function (req, res){
-
-    console.log("REQ DOT", req.body);
+app.post("/blogs", function(req, res) {
+  console.log("REQ DOT", req.body);
   let name = req.body.name;
-  
-  
+
   let year = req.body.year;
   let medium = req.body.medium;
   let source = req.body.source;
   let description = req.body.desc;
-  let newBlog = {name:name, year:year, medium:medium, source:source, description:description};
+  let newBlog = {
+    name: name,
+    year: year,
+    medium: medium,
+    source: source,
+    description: description
+  };
 
   Blog.create(newBlog, (err, blog) => {
     if (err) {
